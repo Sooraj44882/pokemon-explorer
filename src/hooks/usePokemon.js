bd3fea0d51d63841a0805592e889d2fc5d2df8e6
Sooraj44882 <sooraj44882@gmail.com>
2026-06-14 21:13:04 +0530

    added the load more logic

--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -3,17 +3,40 @@ import { useState, useEffect } from 'react';
 export function usePokemon() {
   //  state to hold data
   const [pokemonList, setPokemonList] = useState([]);
+  const [nextUrl, setNextUrl]=useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+  const [loading, setLoading]=useState(false);
 
+  const fetchPokemon=async()=>{
+    if(!nextUrl || loading) return;
+
+    setLoading(true);
+    try{
+      const res=await fetch(nextUrl);
+      const data=await res.json();
+
+      setPokemonList((prev) => {
+        const uniqueNewPokemon = data.results.filter(
+          (newPoke) => !prev.some((prevPoke) => prevPoke.name === newPoke.name)
+        );
+        return [...prev, ...uniqueNewPokemon];
+      });
+      
+      setNextUrl(data.next);
+    } catch (err) {
+      console.error("Failed to fetch pokemon:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
   useEffect(() => {
-    // function goes to the internet and gets the first 20 Pokemon
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
-      .then(response => response.json())
-      .then(data => {
-        console.log("Here is the raw data:", data.results);
-        setPokemonList(data.results);
-      })
-      .catch(error => console.error("Error fetching data:", error));
+    if (pokemonList.length===0){
+      fetchPokemon();
+    }
   }, []);
 
-  return { pokemonList };
+  return { pokemonList,
+    loadMore:fetchPokemon,
+    loading,
+    hasMore:!!nextUrl
+   };
 }
--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -2,24 +2,31 @@ import { usePokemon } from '../hooks/usePokemon';
 import PokemonCard from '../components/PokemonCard';
 
 export default function Pokedex() {
-  const { pokemonList } = usePokemon();
+  const { pokemonList,loadMore,loading,hasMore } = usePokemon();
 
   return (
-    <div className="w-full pb-10">
-      <h2 className="text-4xl font-bold text-yellow-400 mb-8 text-center mt-6 drop-shadow-sm">
+    <div className="w-full bg-white">
+      <h2 className="text-4xl font-bold text-black mb-8 text-center mt-6 ">
         The Pokédex
       </h2>
       
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-        {pokemonList.map((pokemon) => (
-          <PokemonCard key={pokemon.name} pokemon={pokemon} />
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-6">
+        {pokemonList.map((p) => (
+          <PokemonCard key={p.name} pokemon={p} />
         ))}
       </div>
 
-      <button className="bg-blue-600 hover:bg-blue-500 text-white text-center font-bold py-3 px-8 rounded-full shadow-lg transition-transform transform hover:scale-105 cursor-pointer"
-        onClick={() => console.log("Button clicked! , add logic later.")}>
-        Load More Pokémon
-      </button>
+      {hasMore && (
+        <div className="flex justify-center mt-8">
+        <button 
+          disabled={loading}
+          onClick={loadMore}
+          className="px-8 py-3 font-bold text-white transition-all bg-blue-600 rounded-full shadow-lg hover:bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed hover:-translate-y-1"
+        >
+          {loading ? 'Loading...' : 'Load More Pokémon'}
+        </button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
