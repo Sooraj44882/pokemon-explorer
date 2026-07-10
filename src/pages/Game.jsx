3904af9b189470e4f8298014361d1b8d394f269d
Sooraj44882 <sooraj44882@gmail.com>
2026-07-10 23:27:01 +0530

    started making game guess who is this pokemon

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,8 +1,41 @@
-export default function Game() {
-  return (
-    <div className="flex flex-col items-center justify-center mt-20 text-center">
-      <h2 className="mb-4 text-4xl font-extrabold text-blue-600">Who's That Pokémon?</h2>
-      <p className="text-lg text-gray-500">The guessing game is coming soon...</p>
+import { useState } from "react";
+
+export default function Game(){
+  const[mysteryPokemon,setMysteryPokemon]=useState(null);
+
+  const testFetch=async()=>{
+    try{
+      const res=await fetch(`https://pokeapi.co/api/v2/pokemon/1`);
+      const data=await res.json();
+
+      console.log(data.name);
+      setMysteryPokemon(data);
+    }catch(err){
+      console.log("error",err);
+    }
+  };
+
+  return(
+    
+    <div className="p-8 mt-10 bg-white border-4 border-black rounded-xl max-w-xl mx-auto">
+      <h2 className="text-2xl font-bold text-center mb-4">
+        Who's That Pokémon?
+      </h2>
+      
+      <div className="text-center">
+        <button 
+          onClick={testFetch}
+          className="px-4 py-2 bg-blue-500 text-white font-bold border-2 border-black"
+        >
+           Pokémon
+        </button>
+
+        {mysteryPokemon && (
+          <p className="mt-4 font-mono text-green-600">
+            Loaded: {mysteryPokemon.name}
+          </p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
